419375ca7fa6ffba6645ecf51d686e93e207db27
dhanasekhar16 <sekhardhana529@gmail.com>
2019-10-11 00:59:23 +0530

    populate login db

--- course-registration-api/model/models.js
+++ course-registration-api/model/models.js
@@ -110,9 +110,42 @@ Registration.init({
     freezeTableName: true,
 })
 
+class StudentLogin extends Model { } 
+StudentLogin.init({
+    studentId: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+    },
+    password: {
+        type: Sequelize.STRING,
+    },
+}, {
+    sequelize,
+    modelName: 'studentLogin',
+    freezeTableName: true,
+})
+
+class FacultyLogin extends Model { } 
+FacultyLogin.init({
+    facultyId: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+    },
+    password: {
+        type: Sequelize.STRING,
+    },
+}, {
+    sequelize,
+    modelName: 'facultyLogin',
+    freezeTableName: true,
+})
 Faculty.hasMany(Course, { foreignKey: Course.facultyId, sourceKey: Faculty.id, onDelete: 'cascade' })
 Student.hasMany(Registration, { foreignKey: Registration.studentId, sourceKey: Student.id, onDelete: 'cascade' })
 Course.hasMany(Registration, { foreignKey: Registration.courseId, sourceKey: Course.id, onDelete: 'cascade' })
+Student.hasOne(StudentLogin, {foreignKey: StudentLogin.studentId, sourceKey: Student.id, onDelete: 'cascade'})
+Faculty.hasOne(FacultyLogin, {foreignKey: FacultyLogin.facultyId, sourceKey: Faculty.id, onDelete: 'cascade'})
+
+
 
 let fun = (num) => {
     return num > 4 ? num - 5 : num
@@ -130,7 +163,37 @@ let sync = async () => {
     // }
     await Course.sync({ force: false })
     await Student.sync({ force: false })
-    await Registration.sync({ force: true })
+    await Registration.sync({ force: false })
+    await StudentLogin.sync({force: false})
+    await FacultyLogin.sync({force: false})
+
+    // for(let i = 1; i <= 10; i++) {
+    //     FacultyLogin.create({
+    //         facultyId: String(i),
+    //         password: String(i)
+    //     })
+    // }
+
+    // for(let i = 2; i <= 10; i++) {
+    //     Student.create({
+    //         id: String(100+i),
+    //         firstName: 'fname'+i,
+    //         middleName: 'mname'+i,
+    //         lastName: 'lname'+i,
+    //         dept: dept[Math.floor(fun(Math.random()*10))],
+    //         cgpa: (Math.random()*10).toFixed(2),
+    //         semester: 1,
+    //         email: 'fname'+i+'@mail.com',
+    //         sex: 'M'
+    //     })
+    // }
+
+    // for(let i = 1; i <= 10; i++) {
+    //     StudentLogin.create({
+    //         studentId: String(100+i),
+    //         password: String(100+i)
+    //     })
+    // }
 
 }
 
